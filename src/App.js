580e45bb6658435a658dcc05fa97b778bf78298d
Lillian Paris <lillian.paris529@gmail.com>
2022-02-22 10:08:30 -0700

    Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer"
 
 function App() {
   return (
     <>
     <Navbar />
   <h1>Hello World</h1>
+  <Footer />
   </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import "materialize-css";
+import "../Footer/style.css";
+
+
+
+function Footer() {
+    return (
+
+        <>
+            <footer class="page-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col l6 s12">
+                            <h5 class="white-text">Tiffany Sutton Crafts</h5>
+                            <p class="grey-text text-lighten-4">[Here is some more information about me]</p>
+                        </div>
+                        <div class="col l4 offset-l2 s12">
+                            <h5 class="white-text">Lets Connect!</h5>
+                            <ul>
+                                <li><a class="grey-text text-lighten-3" href="#!"><i class="material-icons">email</i></a></li>
+                                {/* <h1>Email</h1> */}
+                                <li><a class="grey-text text-lighten-3" href="#!"><i class="material-icons">add_shopping_cart</i></a></li>
+                                <li><a class="grey-text text-lighten-3" href="#!"><i class="material-icons">camera_alt
+                                </i></a></li>
+                                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                        © 2022 Copyright Text
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
