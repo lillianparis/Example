bf2eab2562eab0d6be0c4c6bce15e0042ebe112c
Lillian Paris <lillian.paris529@gmail.com>
2022-02-22 09:39:20 -0700

    Added footer

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import "materialize-css";
 import "../Navbar/style.css";
 
-function Navbar() {
-    return (
-        <>
+export class Navbar extends Component {
+    componentDidMount() {
 
-            <nav>
-                <div class="nav-wrapper">
-                    <a href="#!" class="brand-logo">Logo</a>
-                    <a href="#Navbar" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                    <ul class="right hide-on-med-and-down">
-                        <li><a href="sass.html">About Me</a></li>
-                        <li><a href="badges.html">Crafts</a></li>
-                        <li><a href="collapsible.html">Contact</a></li>
-                        {/* <li><a href="mobile.html">Mobile</a></li> */}
-                    </ul>
-                </div>
-            </nav>
+        document.addEventListener('DOMContentLoaded', function () {
+            var elems = document.querySelectorAll('.sidenav');
+            var instances = M.Sidenav.init(elems, options);
+            instances();
+        })
+    }
+    render() {
+        return (
+            <>
 
-            <ul class="sidenav" id="mobile-demo">
-                <li><a href="sass.html">About Me</a></li>
-                <li><a href="badges.html">Components</a></li>
-                <li><a href="collapsible.html">Javascript</a></li>
-                <li><a href="mobile.html">Mobile</a></li>
-            </ul>
-        </>
-    )
+                <nav>
+                    <div class="nav-wrapper">
+                        <a href="#!" class="brand-logo">Logo</a>
+                        <a href="#Navbar" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                        <ul class="right hide-on-med-and-down">
+                            <li><a href="sass.html">About Me</a></li>
+                            <li><a href="badges.html">Crafts</a></li>
+                            <li><a href="collapsible.html">Contact</a></li>
+                            {/* <li><a href="mobile.html">Mobile</a></li> */}
+                        </ul>
+                    </div>
+                </nav>
+
+                <ul class="sidenav" id="mobile-demo">
+                    <li><a href="sass.html">About Me</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">Javascript</a></li>
+                    <li><a href="mobile.html">Mobile</a></li>
+                </ul>
+            </>
+        )
+    }
 }
 
 export default Navbar;
