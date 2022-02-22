dd5b94f5c9f073a6d8893d84b0fcb84234af0fba
Lillian Paris <lillian.paris529@gmail.com>
2022-02-22 11:07:05 -0700

    ADded pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer"
+import Footer from "./Components/Footer/Footer";
+import Homepage from "./Pages/Homepage/Homepage";
 
 function App() {
   return (
     <>
-    <Navbar />
-  <h1>Hello World</h1>
-  <Footer />
-  </>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Homepage = () => {
+    return (
+    <>
+    <h1>Helllloooo Worlllldddd</h1>
+    </>
+    )
+}
+
+export default Homepage;
