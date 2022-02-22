42d1f93e779a191bb82e02c1cdc54751cba7ee7e
Lillian Paris <lillian.paris529@gmail.com>
2022-02-22 13:44:25 -0700

    Added

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import Parallax1 from "../Images/parallax1.jpg"
 
 
 const Homepage = () => {
     return (
-    <>
-    <h1>Helllloooo Worlllldddd</h1>
-    </>
+        <>
+            <h1>Helllloooo Worlllldddd</h1>
+            <div class="parallax-container">
+                <div class="parallax"><img src={Parallax1}/></div>
+            </div>
+            <div class="section white">
+                <div class="row container">
+                    <h2 class="header">Parallax</h2>
+                    <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+                </div>
+            </div>
+            <div class="parallax-container">
+                <div class="parallax"><img src={Parallax1}/></div>
+            </div>
+        </>
     )
 }
 
