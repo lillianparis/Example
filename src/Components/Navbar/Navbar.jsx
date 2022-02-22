35c59731cb446e845c6aa4dc3cbf72baee250952
Lillian Paris <lillian.paris529@gmail.com>
2022-02-22 10:19:31 -0700

    Added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,22 +18,22 @@ export class Navbar extends Component {
 
                 <nav>
                     <div class="nav-wrapper">
-                        <a href="#!" class="brand-logo">Logo</a>
+                        <a href="#!" class="brand-logo">Tiffany Sutton Crafts</a>
                         <a href="#Navbar" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                         <ul class="right hide-on-med-and-down">
                             <li><a href="sass.html">About Me</a></li>
                             <li><a href="badges.html">Crafts</a></li>
-                            <li><a href="collapsible.html">Contact</a></li>
-                            {/* <li><a href="mobile.html">Mobile</a></li> */}
+                            <li><a href="collapsible.html">How to</a></li>
+                            <li><a href="mobile.html">Contact</a></li>
                         </ul>
                     </div>
                 </nav>
 
                 <ul class="sidenav" id="mobile-demo">
                     <li><a href="sass.html">About Me</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">Javascript</a></li>
-                    <li><a href="mobile.html">Mobile</a></li>
+                    <li><a href="badges.html">Crafts</a></li>
+                    <li><a href="collapsible.html">How to</a></li>
+                    <li><a href="mobile.html">Contact</a></li>
                 </ul>
             </>
         )
