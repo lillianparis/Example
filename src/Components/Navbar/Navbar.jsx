b422e0bc955c12e172802306761198e76c9877d7
Lillian Paris <lillian.paris529@gmail.com>
2022-02-22 09:25:09 -0700

    Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
             <nav>
                 <div class="nav-wrapper">
                     <a href="#!" class="brand-logo">Logo</a>
-                    <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                    <a href="#Navbar" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                     <ul class="right hide-on-med-and-down">
                         <li><a href="sass.html">About Me</a></li>
                         <li><a href="badges.html">Crafts</a></li>
