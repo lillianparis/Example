4312fe5ea323cbbedf44b0f494d25101b3f26a91
Lillian Paris <lillian.paris529@gmail.com>
2022-03-03 11:49:37 -0700

    Added cards

--- src/Components/ShopCards/ShopCards.jsx
+++ src/Components/ShopCards/ShopCards.jsx
@@ -17,92 +17,165 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Placeholder from "../../Pages/Images/placeholder.jpg";
 import "../ShopCards/style.css";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 
 const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
 }));
 
 export default function ShopCard() {
-  const [expanded, setExpanded] = React.useState(false);
+    const [expanded, setExpanded] = React.useState(false);
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
-  return (
-      <Container>
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Cricket Crafting"
-        subheader="March 1, 2022"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={Placeholder}
-        alt="Placeholder Image"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the mussels,
-          if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. 
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute.
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
-    </Container>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardHeader
+                        avatar={
+                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                R
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title="Cricket Crafting"
+                        subheader="March 1, 2022"
+                    />
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image={Placeholder}
+                        alt="Placeholder Image"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            This impressive paella is a perfect party dish and a fun meal to cook
+                            together with your guests. Add 1 cup of frozen peas along with the mussels,
+                            if you like.
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                        <IconButton aria-label="share">
+                            <ShareIcon />
+                        </IconButton>
+                        <ExpandMore
+                            expand={expanded}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </ExpandMore>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>Method:</Typography>
+                            <Typography paragraph>
+                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+                                aside for 10 minutes.
+                            </Typography>
+                            <Typography paragraph>
+                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+                                medium-high heat.
+                            </Typography>
+                            <Typography paragraph>
+                                Add rice and stir very gently to distribute.
+                            </Typography>
+                            <Typography>
+                                Set aside off of the heat to let rest for 10 minutes, and then serve.
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+                </Col>
+                {/* Next Card */}
+                <Col>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardHeader
+                        avatar={
+                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                                R
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton aria-label="settings">
+                                <MoreVertIcon />
+                            </IconButton>
+                        }
+                        title="Cricket Crafting"
+                        subheader="March 1, 2022"
+                    />
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image={Placeholder}
+                        alt="Placeholder Image"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            This impressive paella is a perfect party dish and a fun meal to cook
+                            together with your guests. Add 1 cup of frozen peas along with the mussels,
+                            if you like.
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                        <IconButton aria-label="share">
+                            <ShareIcon />
+                        </IconButton>
+                        <ExpandMore
+                            expand={expanded}
+                            onClick={handleExpandClick}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </ExpandMore>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography paragraph>Method:</Typography>
+                            <Typography paragraph>
+                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+                                aside for 10 minutes.
+                            </Typography>
+                            <Typography paragraph>
+                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+                                medium-high heat.
+                            </Typography>
+                            <Typography paragraph>
+                                Add rice and stir very gently to distribute.
+                            </Typography>
+                            <Typography>
+                                Set aside off of the heat to let rest for 10 minutes, and then serve.
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
