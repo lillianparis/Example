c6056c91bc6a12e0a65090b255a4be97d9809e2a
Lillian Paris <lillian.paris529@gmail.com>
2022-03-03 11:41:03 -0700

    Successfully added a shopping card

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 // import Parallax1 from "../Images/parallax1.jpg";
 import { Card, Container, Row, Button } from 'react-bootstrap';
+import ShopCard from "../../Components/ShopCards/ShopCards";
 import Dial from "../../Components/SpeedDial/SpeedDial"
 import "../Homepage/style.css";
-
 
 const Homepage = () => {
 
@@ -25,26 +25,9 @@ const Homepage = () => {
                 </Card>
                 </Row>
             </Container>
-            <h1>Crafts</h1>
+            <h1>Shop Some Products</h1>
+            <ShopCard />
 
-            {/* <Container>
-                <Row xs={6} md={4} className="g-4">
-                    {Array.from({ length: 4 }).map((_, idx) => (
-                        <Col>
-                            <Card>
-                                <Card.Img variant="top" src="holder.js/100px160" />
-                                <Card.Body>
-                                    <Card.Title>Card title</Card.Title>
-                                    <Card.Text>
-                                        This is a longer card with supporting text below as a natural
-                                        lead-in to additional content. This content is a little bit longer.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            </Container> */}
 
             <Dial />
 
