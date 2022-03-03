e59f966abccbc4c624567ccae743ca06d75c0a44
Lillian Paris <lillian.paris529@gmail.com>
2022-03-03 13:09:17 -0700

    Added Form

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
-
-const Contact = () => {
-    return (
-        <h2> Contact Page </h2>
-    )
+import { useForm, ValidationError } from '@formspree/react';
+function Contact() {
+  const [state, handleSubmit] = useForm("xgedyzlv");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
 }
-
-export default Contact;
+function App() {
+  return (
+    <Contact />
+  );
+}
+export default App;
