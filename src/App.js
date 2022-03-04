d5c50db1dacf1467369dddc1eff6f03e781774c7
Lillian Paris <lillian.paris529@gmail.com>
2022-03-04 12:25:03 -0700

    Trying to add particle custom

--- src/App.js
+++ src/App.js
@@ -11,9 +11,35 @@ import ParticlesBg from 'particles-bg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  let config = {
+    num: [4, 7],
+    rps: 0.1,
+    radius: [5, 40],
+    life: [1.5, 3],
+    v: [2, 3],
+    tha: [-40, 40],
+    // body: "./img/icon.png", // Whether to render pictures
+    // rotate: [0, 20],
+    alpha: [0.6, 0],
+    scale: [1, 0.1],
+    position: {x:1, y:1, width: 1000, height: 1000}, // all or center or {x:1,y:1,width:100,height:100}
+    color: ["random", "#ff0000"],
+    cross: "dead", // cross or bround
+    random: 15,  // or null,
+    g: 5,    // gravity
+    // f: [2, -1], // force
+    onParticleUpdate: (ctx, particle) => {
+        ctx.beginPath();
+        ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+        ctx.fillStyle = particle.color;
+        ctx.fill();
+        ctx.closePath();
+        console.log("ami working");
+    }
+  };
   return (
     <>
-      <ParticlesBg num={1} type="color" bg={true} />
+      <ParticlesBg num={1} config={config} type="color" bg={true} />
       <Navbar />
       <Router>
         <Routes>
