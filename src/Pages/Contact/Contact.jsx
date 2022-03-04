d66283cea327094ad07815f767f468812bfc65e1
Lillian Paris <lillian.paris529@gmail.com>
2022-03-04 08:54:03 -0700

    Added modern button to contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import Container from "react-bootstrap/Container";
 import Dial from "../../Components/SpeedDial/SpeedDial";
 import { useForm, ValidationError } from '@formspree/react';
 import "../Contact/style.css";
+import Button from '@mui/material/Button';
 function Contact() {
     const [state, handleSubmit] = useForm("xgedyzlv");
     if (state.succeeded) {
@@ -10,41 +11,45 @@ function Contact() {
     }
     return (
         <>
-        <div class="hello">
-            <Container>
-                <h1>Contact Forum</h1>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">
-                        Email Address
-                    </label>
-                    <input
-                        id="email"
-                        type="email"
-                        name="email"
-                    />
-                    <ValidationError
-                        prefix="Email"
-                        field="email"
-                        errors={state.errors}
-                    />
-                      <label htmlFor="email">
-                       Leave a Message
-                    </label>
-                    <textarea
-                        id="message"
-                        name="message"
-                    />
-                    <ValidationError
-                        prefix="Message"
-                        field="message"
-                        errors={state.errors}
-                    />
-                    <button type="submit" disabled={state.submitting}>
+            <div class="hello">
+                <Container>
+                    <h1>Contact Forum</h1>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="email">
+                            Email Address
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                        />
+                        <ValidationError
+                            prefix="Email"
+                            field="email"
+                            errors={state.errors}
+                        />
+                        <label htmlFor="email">
+                            Leave a Message
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                        />
+                        <ValidationError
+                            prefix="Message"
+                            field="message"
+                            errors={state.errors}
+                        />
+                        {/* <button type="submit" disabled={state.submitting}>
                         Submit
-                    </button>
-                </form>
+                    </button> */}
 
-            </Container>
+                        <Button variant="contained" size="medium" type="submit" disabled={state.submitting}>
+                            Send
+                        </Button>
+                    </form>
+
+                </Container>
             </div>
             <Dial />
         </>
